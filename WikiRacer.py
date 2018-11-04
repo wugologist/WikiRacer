0b1fc5b6623c5520fc478c93a29218b3e876b0ca
Kevin Johnson <kevin@coolkev.com>
2018-11-04 16:28:46 -0500

    Improvements to WikiRacer.py (#8)
    * Updated logging configuration
    
    https://stackoverflow.com/a/46098711/1556343
    
    * Added Shebang
    
    * Raise an exception too if the heuristic is invalid
    
    * Squash Bugs
    
    * Make argument heuristic required and provide valid options in the help message
    
    * Update README.md
    
    * Changed arguments to --no-X instead of --only-X

--- WikiRacer.py
+++ WikiRacer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import datetime
 import logging
@@ -10,15 +12,18 @@ import WikipediaApi
 
 log = logging.getLogger(__name__)
 
+def getValidHeuristics():
+    return list(filter(lambda x: x[0] != "_", dir(Heuristics))) # filter out dunder methods
 
 def parse(argv):
     parser = argparse.ArgumentParser(description="Find a path between Wikipedia pages with the given heuristic",
                                      prog=argv[0])
     parser.add_argument("start", help="The starting article name")
     parser.add_argument("goal", help="The goal page name")
-    parser.add_argument("--heuristic", help="The heuristic function to use (from Heuristics.py)")
+    parser.add_argument("--heuristic", help="The heuristic function to use. One of {}".format(getValidHeuristics()), required=True)
     parser.add_argument("--quiet", "-q", help="Create fewer log messages", action="count")
-    parser.add_argument("--nolog", "-n", help="Log to console instead of file", action="store_true")
+    parser.add_argument("--no-console", help="Disable console logging", action="store_true")
+    parser.add_argument("--no-file", help="Disable file logging", action="store_true")
 
     return parser.parse_args(argv[1:])
 
@@ -28,27 +33,29 @@ def initialize_logger(arguments):
     levels = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR]
     level = levels[min(arguments.quiet, len(levels))] if arguments.quiet else 0
 
-    if arguments.nolog:
-        logging.basicConfig(
-            format=format,
-            level=level)
-    else:
+    handlers = []
+
+    if not arguments.no_file:
         if not os.path.exists("logs/"):
             os.makedirs("logs/")
+        filename = "logs/{}.log".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        handlers.append(logging.FileHandler(filename))
 
-        logging.basicConfig(
-            format=format,
-            level=level,
-            filename="logs/{}.log".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")),
-            filemode="a")
+    if not arguments.no_console:
+        handlers.append(logging.StreamHandler(sys.stdout))
+
+    logging.basicConfig(
+        format=format,
+        level=level,
+        handlers=handlers)
 
 
 def run_search(arguments):
     heuristic = getattr(Heuristics, arguments.heuristic, None)
+
     if heuristic is None:
-        log.error("{} is not a valid heuristic. Options: {}"
-                  .format(arguments.heuristic,
-                          list(filter(lambda x: x[0] != "_", dir(Heuristics)))))  # filter out dunder methods
+        log.error("{} is not a valid heuristic. Options: {}".format(arguments.heuristic, getValidHeuristics()))
+        raise ValueError("Invalid heuristic")
 
     api = WikipediaApi.WikipediaApi()
     start = api.get_canonical_name(arguments.start)
