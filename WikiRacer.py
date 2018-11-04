21f4663c78dbf481adbdc6dde064e5b01380485e
Kevin Johnson <kevin@coolkev.com>
2018-11-04 15:32:15 -0500

    Make argument heuristic required and provide valid options in the help message

--- WikiRacer.py
+++ WikiRacer.py
@@ -12,13 +12,15 @@ import WikipediaApi
 
 log = logging.getLogger(__name__)
 
+def getValidHeuristics():
+    return list(filter(lambda x: x[0] != "_", dir(Heuristics))) # filter out dunder methods
 
 def parse(argv):
     parser = argparse.ArgumentParser(description="Find a path between Wikipedia pages with the given heuristic",
                                      prog=argv[0])
     parser.add_argument("start", help="The starting article name")
     parser.add_argument("goal", help="The goal page name")
-    parser.add_argument("--heuristic", help="The heuristic function to use (from Heuristics.py)")
+    parser.add_argument("--heuristic", help="The heuristic function to use. One of {}".format(getValidHeuristics()), required=True)
     parser.add_argument("--quiet", "-q", help="Create fewer log messages", action="count")
     parser.add_argument("--only-console", help="Log to only the console", action="store_true")
     parser.add_argument("--only-file", help="Log to only a file", action="store_true")
@@ -52,9 +54,7 @@ def run_search(arguments):
     heuristic = getattr(Heuristics, arguments.heuristic, None)
 
     if heuristic is None:
-        log.error("{} is not a valid heuristic. Options: {}"
-                  .format(arguments.heuristic,
-                          list(filter(lambda x: x[0] != "_", dir(Heuristics)))))  # filter out dunder methods
+        log.error("{} is not a valid heuristic. Options: {}".format(arguments.heuristic, getValidHeuristics()))
         raise ValueError("Invalid heuristic") 
 
     api = WikipediaApi.WikipediaApi()
