a9e849728380c57e9bb3526b781a19604205dd27
Michael Wong <emporophobe@gmail.com>
2018-11-19 14:32:58 -0500

    Partial implementation

--- src/heuristics/Heuristics.py
+++ src/heuristics/Heuristics.py
@@ -1,3 +1,7 @@
+from src import WikipediaApi
+from src.heuristics import TFIDF
+
+
 def null_heuristic(node, start, goal, content, neighbors):
     return 0
 
@@ -16,3 +20,27 @@ def dfs_heuristic(node, start, goal, content, neighbors):
 
 def shortcut_dfs_heuristic(node, start, goal, content, neighbors):
     return -float("inf") if node == goal else -1
+
+
+def tfidf_heuristic(node, start, goal, content, neighbors):
+    return Tfidf.get_instance(goal, "corpera/1000.txt").get_heuristic_value(node)
+
+
+class Tfidf:
+    instance = None
+
+    def __init__(self, goal, corpus_filename):
+        self.tfidf = TFIDF.Tfidf(corpus_filename)
+        goal_text = WikipediaApi.WikipediaApi().get_text_and_links(goal)[0]
+        self.goal_transform = self.tfidf.get_transform(goal_text)
+
+    @staticmethod
+    def get_instance(goal, corpus_filename):
+        if Tfidf.instance is None:
+            instance = Tfidf(goal, corpus_filename)
+        return instance
+
+    def get_heuristic_value(self, node):
+        node_text = WikipediaApi.WikipediaApi().get_text_and_links(node)[0]
+        node_transform = self.tfidf.get_transform(node_text)
+        return self.tfidf.compare_transforms(node_transform, self.goal_transform)
--- src/heuristics/TFIDF.py
+++ src/heuristics/TFIDF.py
@@ -4,8 +4,6 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from src import WikipediaApi
-
 
 class Tfidf:
     def __init__(self, corpus_filename):
@@ -25,19 +23,32 @@ class Tfidf:
     def get_transform(self, document):
         return self.tfidf.transform([document])
 
-    def extract_top_n_terms(self, document, n=None):
-        transform = self.get_transform(document)
+    def extract_top_terms(self, transform, limit=None):
+        # transform = self.get_transform(document)
         feature_names = self.tfidf.get_feature_names()
         terms = [(feature_names[term], transform[0, term]) for term in transform.nonzero()[1]]
         sorted_terms = sorted(terms, key=lambda t: t[1], reverse=True)
-        return sorted_terms[:n + 1] if n is not None else sorted_terms
+        return sorted_terms[:limit + 1] if limit is not None else sorted_terms
+
+    def compare_transforms(self, transform1, transform2):
+        """
+        Calculate the similarity between two transform matrices
+        :param transform1:
+        :param transform2:
+        :return: A number in [0, 1] where 0 is a perfect match
+        """
+        t1_terms = {t[0] for t in self.extract_top_terms(transform1, 20)}
+        t2_terms = {t[0] for t in self.extract_top_terms(transform2, 20)}
+
+        return 1 - len((t1_terms.intersection(t2_terms))) / len(t1_terms)
 
 
 if __name__ == "__main__":
     tfidf = Tfidf("../../corpera/1000.txt")
     print("stuff:")
-    response = tfidf.get_transform("This is a random string, whatever, blue, purple, portmanteau")
-    print(tfidf.extract_top_n_terms("this is a super important document fdsfdsa the in a", 5))
-    api = WikipediaApi.WikipediaApi()
-    text = api.get_random_text_and_links()[0]
-    print(tfidf.extract_top_n_terms(text, 10))
+    t1 = tfidf.get_transform("test document is a test")
+    t2 = tfidf.get_transform("other document is a text")
+    print(tfidf.compare_transforms(t1, t2))
+    print(tfidf.compare_transforms(t1, t1))
+    print(tfidf.compare_transforms(t2, t1))
+    print(tfidf.compare_transforms(t2, t2))
