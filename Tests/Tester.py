47c28c7492db750d4932e76f4c40e19e053dd78e
Li Breite <lbreite@ccs.neu.edu>
2018-11-27 15:50:27 -0500

    Write results to TSV

--- Tests/Tester.py
+++ Tests/Tester.py
@@ -4,7 +4,7 @@ from src import HeuristicTester
 from src.heuristics import Heuristics
 
 test_file_path = "tests.txt"
-results_file_path = "results.txt"
+results_file_path = "results.tsv"
 
 heuristics = [Heuristics.BfsHeuristic, Heuristics.DfsHeuristic]
 
@@ -19,23 +19,24 @@ def run_tests():
                 tests.append((parts[0], parts[1]))
 
     with open(results_file_path, "w") as results_file:
-        results_file.write(str(datetime.datetime.now()) + "\n\n")
+        results_file.write("start\tgoal\theuristic\ttime_seconds\tnodes_expanded\tpath_length\tpath\n")
 
         for test in tests:
             start, goal = test
-            results_file.write("Start: " + start + ", Goal: " + goal + "\n")
+            # results_file.write("Start: " + start + ", Goal: " + goal + "\n")
             results_file.writelines(run_one_test(start, goal))
-            results_file.write("\n")
-    pass
+            # results_file.write("\n")
+        results_file.write(str(datetime.datetime.now()))  # do we want a timestamp? where should it go?
+
+
 
 
 def run_one_test(start, goal):
     res = HeuristicTester.HeuristicTester.compare_heuristics(start, goal, heuristics)
     ans_list = []
     for r in res:
-        line = "Heuristic: {}, Time(in seconds): {}, Nodes expanded: {}, Path length: {}, Path: {}\n" \
-            .format(r["heuristic"], r["time_seconds"], r["nodes_expanded"], r["path_length"], r["path"])
-        ans_list.append(line)
+        line = [start, goal, r["heuristic"], str(r["time_seconds"]), str(r["nodes_expanded"]), str(r["path_length"]), str(r["path"])]
+        ans_list.append("\t".join(line) + "\n")
     return ans_list
 
 
